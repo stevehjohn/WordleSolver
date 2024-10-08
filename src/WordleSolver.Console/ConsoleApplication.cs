5228d915637dd294b75589808855e4b81b479690
Stevo John <stevehuwjohn@gmail.com>
2024-10-08 19:36:35 +0100

    WIP

---
+++ src/WordleSolver.Console/ConsoleApplication.cs
@@ -0,0 +1,86 @@
+using WordleSolver.Infrastructure;
+using static System.Console;
+
+namespace WordleSolver.Console;
+
+public class ConsoleApplication
+{
+    private readonly WordList _wordList = new(WordSet.Basic);
+    
+    public void Run()
+    {
+        ShowWelcome();
+
+        while (true)
+        {
+            ExecuteRound();
+        }
+        // ReSharper disable once FunctionNeverReturns
+    }
+
+    private void ExecuteRound()
+    {
+        OutputLine("Enter any correctly placed letters separated by spaces or dots:");
+        
+        Output("> ");
+
+        var correct = ReadLine();
+
+        OutputLine("Enter any incorrectly placed letters:");
+        
+        Output("> ");
+
+        var incorrect = ReadLine();
+
+        OutputLine("Enter any excluded letters:");
+        
+        Output("> ");
+
+        var excluded = ReadLine();
+        
+        OutputLine("Suggestions:");
+
+        var result = _wordList.GetMatches(correct, incorrect, excluded).Take(10);
+        
+        OutputLine($"{string.Join(", ", result)}");
+    }
+
+    private static void ShowWelcome()
+    {
+        OutputLine("Welcome to Wordle Solver!");
+        
+        OutputLine("When entering correctly or placed letters,");
+        
+        OutputLine("separate with hypens to indicate the position.", false);
+
+        OutputLine("E.g. at the prompt:");
+        
+        OutputLine("> -A--E");
+        
+        OutputLine("Excluded letters are not position sensitive.");
+        
+        OutputLine(string.Empty, false);
+    }
+
+    private static void OutputLine(string text = null, bool newLineBefore = true)
+    {
+        if (newLineBefore)
+        {
+            WriteLine();
+        }
+        
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            WriteLine();
+        }
+        else
+        {
+            WriteLine($"  {text}");
+        }
+    }
+
+    private static void Output(string text)
+    {
+        Write($"  {text}");
+    }
+}
--- src/WordleSolver.Console/EntryPoint.cs
+++ src/WordleSolver.Console/EntryPoint.cs
@@ -1,51 +1,14 @@
 using System.Diagnostics.CodeAnalysis;
-using WordleSolver.Infrastructure;
-using static System.Console;
 
 namespace WordleSolver.Console;
 
 [ExcludeFromCodeCoverage]
 public static class EntryPoint
 {
-    private static readonly WordList WordList = new(WordSet.Basic);
-    
     public static void Main()
     {
-        WriteLine();
-        
-        WriteLine("  Welcome to Wordle Solver.");
+        var application = new ConsoleApplication();
 
-        while (true)
-        {
-            ExecuteRound();
-        }
-        // ReSharper disable once FunctionNeverReturns
-    }
-
-    private static void ExecuteRound()
-    {
-        WriteLine($"{Environment.NewLine}  Enter any correctly placed letters separated by spaces:");
-        
-        Write("  > ");
-
-        var correct = ReadLine();
-
-        WriteLine($"{Environment.NewLine}  Enter any incorrectly placed letters:");
-        
-        Write("  > ");
-
-        var incorrect = ReadLine();
-
-        WriteLine($"{Environment.NewLine}  Enter any excluded letters:");
-        
-        Write("  > ");
-
-        var excluded = ReadLine();
-        
-        WriteLine($"{Environment.NewLine}  Suggestions:");
-
-        var result = WordList.GetMatches(correct, incorrect, excluded).Take(10);
-        
-        WriteLine($"{Environment.NewLine}  {string.Join(", ", result)}");
+        application.Run();
     }
 }
