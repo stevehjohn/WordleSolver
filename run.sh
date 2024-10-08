057581f2799524837ff8a9565ec53c2e47a7d4c3
Steve John <steve.john@justeattakeaway.com>
2024-10-08 14:25:47 +0100

    Console WIP

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 dotnet build -c Release src/WordleSolver.sln
 
-dotnet src/WordleSolver.Console/bin/Release/net8.0/WordleSolver.Console.dll "$@"
+clear
+
+cd src/WordleSolver.Console/bin/Release/net8.0/
+
+dotnet WordleSolver.Console.dll "$@"
+
+cd -
