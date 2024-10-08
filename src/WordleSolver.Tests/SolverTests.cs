8aadd6887ebc8551f44927e22f66abdbe01365fe
Stevo John <stevehuwjohn@gmail.com>
2024-10-08 23:09:14 +0100

    WIP

--- src/WordleSolver.Tests/SolverTests.cs
+++ src/WordleSolver.Tests/SolverTests.cs
@@ -6,39 +6,41 @@ public class SolverTests
 {
     private readonly Solver _solver = new(WordSet.Basic);
     
-    [Theory]
-    [InlineData("", "i,3|o,4", "aud", "bigot,biome,biros,bison,boils,coils,coins,corgi,envoi,foils,foist,gigot,going,hoist,icons,ingot,irons,irony,ivory," +
-                                      "joins,joint,joist,kilos,kiosk,limos,lions,loins,minor,moist,noise,noisy,olive,omits,onion,opine,oriel,osier,ovine," +
-                                      "owing,picot,pilot,piton,pitot,pivot,point,poise,prior,riots,roily,scion,silos,soils,toile,toils,torsi,trios,visor," +
-                                      "voice,voile,winos")]
-    [InlineData("t,4", "i,1|o,3", "bg", "foist,hoist,joint,joist,moist,point,posit,quoit,vomit")]
-    [InlineData("o,1|i,2|t,4", "", "fs", "joint,point")]
-    public void ReturnsExpectedWordsGivenConditions(string correct, string incorrect, string excluded, string expected)
+    [Fact]
+    public void ReturnsExpectedWordsGivenConditions01()
     {
-        ParseSetupParameter(correct).ForEach(p => _solver.SetCorrect(p.Character, p.Position));
-
-        ParseSetupParameter(incorrect).ForEach(p => _solver.AddIncorrect(p.Character, p.Position));
+        _solver.Reset();
         
-        excluded.ToCharArray().ToList().ForEach(c => _solver.AddExcluded(c));
+        _solver.AddIncorrect('i', 3);
+        _solver.AddIncorrect('o', 4);
+        _solver.AddExcluded('a');
+        _solver.AddExcluded('u');
+        _solver.AddExcluded('d');
 
         var result = _solver.GetMatches();
         
-        Assert.Equal(expected, string.Join(',', result));
-    }
-
-    private static List<Tile> ParseSetupParameter(string parameter)
-    {
-        var split = parameter.Split('|', StringSplitOptions.RemoveEmptyEntries);
-
-        var result = new List<Tile>();
+        Assert.Equal("bigot,biome,biros,bison,boils,coils,coins,corgi,envoi,foils,foist,gigot,going,hoist,icons,ingot,irons,irony,ivory," +
+                     "joins,joint,joist,kilos,kiosk,limos,lions,loins,minor,moist,noise,noisy,olive,omits,onion,opine,oriel,osier,ovine," +
+                     "owing,picot,pilot,piton,pitot,pivot,point,poise,prior,riots,roily,scion,silos,soils,toile,toils,torsi,trios,visor," +
+                     "voice,voile,winos", string.Join(',', result));
         
-        foreach (var item in split)
-        {
-            var parts = item.Split(',');
-            
-            result.Add(new Tile(parts[0][0], int.Parse(parts[1])));
-        }
+        _solver.SetCorrect('t', 4);
+        _solver.AddIncorrect('i', 1);
+        _solver.AddIncorrect('o', 3);
+        _solver.AddExcluded('b');
+        _solver.AddExcluded('g');
 
-        return result;
-    }
+        result = _solver.GetMatches();
+        
+        Assert.Equal("foist,hoist,joint,joist,moist,point", string.Join(',', result));
+        
+        _solver.SetCorrect('o', 1);
+        _solver.SetCorrect('i', 2);
+        _solver.AddExcluded('f');
+        _solver.AddExcluded('s');
+
+        result = _solver.GetMatches();
+         
+        Assert.Equal("joint,point", string.Join(',', result));
+     }
 }
