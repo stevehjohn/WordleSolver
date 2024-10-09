cc25b7ae76b83e76a36c409f32e75b3b3a5d73c1
Stevo John <stevehuwjohn@gmail.com>
2024-10-09 14:37:33 +0100

    WIP

--- src/WordleSolver.Playground/Toys/Excerciser.cs
+++ src/WordleSolver.Playground/Toys/Excerciser.cs
@@ -1,4 +1,5 @@
 using WordleSolver.Infrastructure;
+using static System.Console;
 
 namespace WordleSolver.Playground.Toys;
 
@@ -7,12 +8,45 @@ public class Excerciser
     private readonly WordList _wordList = new(WordSet.Basic);
 
     private readonly Solver _solver = new Solver(WordSet.Basic);
+
+    private int _rounds = 0;
+
+    private int _totalSteps = 0;
+
+    private int _fails = 0;
     
     public void RunAgainstAllWords()
     {
+        WriteLine();
+        WriteLine("  Playing all words!");
+        
         foreach (var word in _wordList.Words)
+        {
+            PlayGame(word);
+        }
+        
+        WriteLine();
+        WriteLine($"  Rounds Played: {_rounds}");
+        WriteLine($"  Average Steps: {(float) _totalSteps / _rounds:F2}");
+        WriteLine($"  Failures:      {_fails}");
+        WriteLine();
+        WriteLine("  Cheers!");
+        WriteLine();
+    }
+
+    private void PlayGame(string expected)
+    {
+        var word = "AUDIO";
+
+        var steps = 0;
+        
+        _solver.Reset();
+        
+        while (true)
         {
             
         }
+
+        _rounds++;
     }
 }
